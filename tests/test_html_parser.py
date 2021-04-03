c6e99c49c61489b45de77f4264dcb8d4f0da1843
jordenk <jordenkawamoto@gmail.com>
2021-04-03 12:08:38 -0500

    rename html_parser and re-arrange test data

--- tests/test_html_parser.py
+++ tests/test_html_parser.py
@@ -1,10 +1,10 @@
-from src.html_builder import collect_html_file_paths
+from src.html_parser import collect_html_file_paths
 import os
 
 
 def test_collect_html_file_paths():
     test_dir_path = os.path.join(os.path.dirname(__file__))
-    test_data_path = os.path.join(test_dir_path, "resources", "html_builder_test_data")
+    test_data_path = os.path.join(test_dir_path, "resources", "html_parser_test_data", "nested_file_structure_data")
 
     actual = collect_html_file_paths(test_data_path, [])
     expected = {
